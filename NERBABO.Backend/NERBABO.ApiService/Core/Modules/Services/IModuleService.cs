7d9222c3260d86225478276c0da1ddae162be02e
bernardo@nerba.pt <code.mmd2@gmail.com>
2025-06-20 16:11:00 +0100

    feat: IGenericService interface that takes a Retrieve, Update, Create Dto and serves as a generic declaration of methods that are the same throw all the services implementations. interface IModuleService now implements this aproach

--- NERBABO.Backend/NERBABO.ApiService/Core/Modules/Services/IModuleService.cs
+++ NERBABO.Backend/NERBABO.ApiService/Core/Modules/Services/IModuleService.cs
@@ -1,16 +1,13 @@
 ﻿using NERBABO.ApiService.Core.Modules.Dtos;
 using NERBABO.ApiService.Core.Modules.Models;
 using NERBABO.ApiService.Shared.Models;
+using NERBABO.ApiService.Shared.Services;
 
 namespace NERBABO.ApiService.Core.Modules.Services
 {
     public interface IModuleService
+        : IGenericService<RetrieveModuleDto, CreateModuleDto, UpdateModuleDto>
     {
-        Task<Result<IEnumerable<RetrieveModuleDto>>> GetAllModulesAsync();
-        Task<Result<RetrieveModuleDto>> GetModuleByIdAsync(long id);
-        Task<Result<RetrieveModuleDto>> CreateModuleAsync(CreateModuleDto moduleDto);
-        Task<Result<RetrieveModuleDto>> UpdateModuleAsync(UpdateModuleDto moduleDto);
-        Task<Result> DeleteModuleAsync(long id);
         Task<Result<IEnumerable<RetrieveModuleDto>>> GetActiveModulesAsync();
     }
 }
---
+++ NERBABO.Backend/NERBABO.ApiService/Shared/Services/IGeneralService.cs
@@ -0,0 +1,17 @@
+﻿using NERBABO.ApiService.Shared.Models;
+
+namespace NERBABO.ApiService.Shared.Services
+{
+    // T is a Retrieve Entity dto
+    // U is a Update Entity dto
+    // C is a Create Entity dto
+    public interface IGenericService<T, C, U>
+    {
+        Task<Result<IEnumerable<T>>> GetAllAsync();
+        Task<Result<T>> GetByIdAsync(long id);
+        Task<Result<T>> CreateAsync(C entityDto);
+        Task<Result<T>> UpdateAsync(U entityDto);
+        Task<Result> DeleteAsync(long id);
+
+    }
+}
