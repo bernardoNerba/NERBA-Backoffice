97153319dd49ae7ce1181cdf0a44da2b12948301
devMotcho <code.mmd2@gmail.com>
2025-09-10 11:53:43 +0100

    feat: Add validation attributes to Create and Update Module Avaliation DTOs

--- NERBABO.Backend/NERBABO.ApiService/Core/ModuleAvaliations/Dtos/UpdateModuleAvaliationDto.cs
+++ NERBABO.Backend/NERBABO.ApiService/Core/ModuleAvaliations/Dtos/UpdateModuleAvaliationDto.cs
@@ -1,8 +1,11 @@
-using System;
+using System.ComponentModel.DataAnnotations;
+using NERBABO.ApiService.Shared.Dtos;
 
 namespace NERBABO.ApiService.Core.ModuleAvaliations.Dtos;
 
-public class UpdateModuleAvaliationDto
+public class UpdateModuleAvaliationDto : EntityDto<long>
 {
-
+    [Required(ErrorMessage = "Grade é obrigatório")]
+    [Range(0, 5, ErrorMessage = "Grade deve estar entre 0 e 5")]
+    public int Grade { get; set; }
 }
